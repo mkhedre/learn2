7366903f7ee2f58db082150f3a0ca4efe88bbbd8
mostafa khedre <mostafa.khedr@innosoft.sa>
2025-04-12 10:39:59 +0200

    feat: add id

--- src/productsList/ProductItem.jsx
+++ src/productsList/ProductItem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const ProductItem = React.memo(({ product, onLike }) => {
-  console.log("Rendering:", product.name);
   return (
     <div style={{ border: "1px solid #ccc", margin: "8px", padding: "8px" }}>
+      <h1>{product.id}</h1>
       <h3>{product.name}</h3>
       <button onClick={() => onLike(product.id)}>Like</button>
     </div>
